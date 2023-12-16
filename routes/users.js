e2617ce39915f9a49d07c591811503cb6d7df66d
Nehal <npatil14@hawk.iit.edu>
2023-12-16 02:37:37 -0600

    feat() : changes

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contentstack = require('contentstack');
-
-router.get('/routing-test', (req, res, next) => {
-    return res.status(200).send(`<h1>All OK, ROUTING TEST SUCCESFULL</h1>`);
-});
-
-module.exports = router;
